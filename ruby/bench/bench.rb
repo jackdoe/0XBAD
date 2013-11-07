45e2114fd2eed2a500a2edb0be41f25efb70a0d4
borislav nikolov <jack@sofialondonmoskva.com>
2013-11-07 22:47:35 +0100

    update readme; add more benchmarks for ruby

--- ruby/bench/bench.rb
+++ ruby/bench/bench.rb
@@ -5,6 +5,9 @@ rescue LoadError
 end
 require 'sereal'
 require 'benchmark/ips'
+require 'memcached'
+cache = Memcached.new("localhost:11211")
+
 n = 1_000_000
 b = XBAD.create(0xBAD,16,128)
 key = "abcdef"
@@ -21,6 +24,12 @@ Benchmark.ips do |x|
     x.report("xbad get") { z = b.find(key) }
     x.report("hash mis") { z = hash[missing] }
     x.report("xbad mis") { z = b.find(missing) }
+    x.report("memcache set") { cache.set(key,value) }
+    x.report("memcache get") { z = cache.get(key) }
+    x.report("memcache cset") { cache.set(key,complex) }
+    x.report("memcache cget") { z = cache.get(key) }
+    x.report("hash clone set") { hash[key] = complex.clone  }
+    x.report("hash clone get") { z = hash[key].clone }
     x.report("xbad set-encoded") { b.store(key,Sereal.encode(complex),0) }
     x.report("xbad get-encoded") { z = Sereal.decode(b.find(key)) }
     x.report("xbad set-encoded-sn") { b.store(key,Sereal.encode(complex,true),0) }
