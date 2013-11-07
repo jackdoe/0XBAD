5e0a3ccec00da8b79a0322e7feddb0e09e4c7160
borislav nikolov <jack@sofialondonmoskva.com>
2013-11-07 23:33:36 +0100

    update bench

--- perl/bench/bench.pl
+++ perl/bench/bench.pl
@@ -3,7 +3,7 @@ use Benchmark ':all';
 use BAD;
 use Cache::Memcached::Fast;
 my $memd = new Cache::Memcached::Fast({servers => [ { address => 'localhost:11211', weight => 2.5 } ]});
-my $beef = BAD->new(0xBEEF,16,128);
+my $bad = BAD->new(0xBEEF,16,128);
 my %hash;
 
 for my $key(qw(a aaaaaaaaaaaaaaaabbbbbbbbbbbbbbb)) {
@@ -11,21 +11,21 @@ for my $key(qw(a aaaaaaaaaaaaaaaabbbbbbbbbbbbbbb)) {
         my $value = "a" x int($i);
         my $r = timethese($count, {
             "$i $key - memcached fast store" => sub { $memd->set($key,$value)},
-            "$i $key - 0xbeef store" => sub { $beef->store($key,$value,0)},
+            "$i $key - 0xbad store" => sub { $bad->store($key,$value,0)},
             "$i $key - native hash store" => sub { $hash{$key} = $value },
         });
         cmpthese($r); 
 
         $r = timethese($count, {
             "$i $key - memcached fast get" => sub { my $x = $memd->get($key)},
-            "$i $key - 0xbeef get" => sub { $x = $beef->find($key)},
+            "$i $key - 0xbad get" => sub { $x = $bad->find($key)},
             "$i $key - native hash get" => sub { my $x = $hash{$key}},
         });
         cmpthese($r); 
         my $missing = $key . "x";
         $r = timethese($count, {
             "$i $key - memcached fast miss" => sub { my $x = $memd->get($missing)},
-            "$i $key - 0xbeef miss" => sub { $x = $beef->find($missing)},
+            "$i $key - 0xbad miss" => sub { $x = $bad->find($missing)},
             "$i $key - native hash miss" => sub { my $x = $hash{$missing}},
         });
         cmpthese($r); 
